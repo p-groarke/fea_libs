2849fd444a8ae6f92711da46cdc95eaa1d7f2cf1
p-groarke <philippe.groarke@gmail.com>
2020-12-29 15:41:08 -0500

    Add random, wide-string exe_dir. (#9)
    * Add random, wide-string exe_dir.
    
    * Fix solution, fix random.

---
+++ include_cpp14/fea/utils/random.hpp
@@ -0,0 +1,54 @@
+﻿#pragma once
+#include "fea/utils/platform.hpp"
+
+#include <chrono>
+#include <limits>
+#include <random>
+#include <type_traits>
+
+namespace fea {
+namespace detail {
+template <size_t Bytes>
+struct platform_mersenne;
+
+template <>
+struct platform_mersenne<4> {
+	using type = std::mt19937;
+};
+
+template <>
+struct platform_mersenne<8> {
+	using type = std::mt19937_64;
+};
+
+using platform_mt19937 =
+		typename detail::platform_mersenne<sizeof(size_t)>::type;
+
+FEA_INLINE_VAR std::random_device rd;
+FEA_INLINE_VAR platform_mt19937 gen(
+		size_t(std::chrono::system_clock::now().time_since_epoch().count()));
+
+} // namespace detail
+
+// Get a random int, from lowest to highest int value.
+template <class T>
+T random_int() {
+	std::uniform_int_distribution<T> dist(
+			std::numeric_limits<T>::lowest(), std::numeric_limits<T>::max());
+	return dist(detail::gen);
+}
+
+// Get a random int between [min, max].
+template <class T>
+T random_int(T min, T max) {
+	std::uniform_int_distribution<T> dist(min, max);
+	return dist(detail::gen);
+}
+
+// Get a random index, from 0 to count - 1.
+inline size_t random_idx(size_t count) {
+	std::uniform_int_distribution<size_t> dist(0, count - 1);
+	return dist(detail::gen);
+}
+
+} // namespace fea
