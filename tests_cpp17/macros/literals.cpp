1d678fd4f8e084fdbbcdf899ef5485c01380d3cc
Philippe Groarke <hello@philippegroarke.com>
2021-12-24 14:48:23 -0500

    ci

--- tests_cpp17/macros/literals.cpp
+++ tests_cpp17/macros/literals.cpp
@@ -3,7 +3,38 @@
 #include <fea/string/conversions.hpp>
 #include <gtest/gtest.h>
 
+#define testme(...) \
+	std::array<std::string, 6> arr \
+			= { FEA_FOR_EACH(FEA_STRINGIFY_COMMA, __VA_ARGS__) }
+
 namespace {
+TEST(macros_literals, basics) {
+	EXPECT_STREQ(FEA_STRINGIFY(test), "test");
+	EXPECT_STREQ(FEA_WSTRINGIFY(test), L"test");
+	EXPECT_EQ(std::u16string{ FEA_U16STRINGIFY(test) }, u"test");
+	EXPECT_EQ(std::u32string{ FEA_U32STRINGIFY(test) }, U"test");
+
+	[](std::string str1, std::string str2) { EXPECT_EQ(str1, str2); }(
+			FEA_STRINGIFY_COMMA(test) "test");
+	[](std::wstring str1, std::wstring str2) { EXPECT_EQ(str1, str2); }(
+			FEA_WSTRINGIFY_COMMA(test) L"test");
+	[](std::u16string str1, std::u16string str2) { EXPECT_EQ(str1, str2); }(
+			FEA_U16STRINGIFY_COMMA(test) u"test");
+	[](std::u32string str1, std::u32string str2) { EXPECT_EQ(str1, str2); }(
+			FEA_U32STRINGIFY_COMMA(test) U"test");
+
+	EXPECT_EQ(FEA_SIZEOF_VAARGS(test, test, test), 3);
+	EXPECT_EQ(FEA_SIZEOF_VAARGS(test, test, test, test, test, test), 6);
+
+	{
+		testme(0, 1, 2, 3, 4, 5);
+
+		for (size_t i = 0; i < arr.size(); ++i) {
+			EXPECT_EQ(arr[i], std::to_string(i));
+		}
+	}
+}
+
 TEST(macros_literals, prefix) {
 	// If this compiles the literals are well formed.
 	std::string t1{ FEA_PREFIX("test") };
