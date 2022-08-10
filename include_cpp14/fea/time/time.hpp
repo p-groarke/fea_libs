806fea99792b085f905aba08433feeddd9fba161
Philippe Groarke <hello@philippegroarke.com>
2022-08-10 14:48:14 -0400

    ci

--- include_cpp14/fea/time/time.hpp
+++ include_cpp14/fea/time/time.hpp
@@ -30,9 +30,12 @@ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 #pragma once
+#include "fea/utils/platform.hpp"
+
+#if FEA_CPP17
 #include "fea/macros/literals.hpp"
 #include "fea/string/string.hpp"
-#include "fea/utils/platform.hpp"
+#endif
 
 #include <array>
 #include <chrono>
