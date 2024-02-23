ddd6ae6a39535b53a9bb944eee451c981f98fb2e
Philippe Groarke <hello@philippegroarke.com>
2024-02-22 20:08:02 -0500

    moving_averages : wip

---
+++ include_cpp17/fea/math/moving_average.hpp
@@ -0,0 +1,193 @@
+/*
+BSD 3-Clause License
+
+Copyright (c) 2023, Philippe Groarke
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+* Neither the name of the copyright holder nor the names of its
+  contributors may be used to endorse or promote products derived from
+  this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+#pragma once
+#include "fea/math/math.hpp"
+
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <cmath>
+#include <type_traits>
+#include <utility>
+
+/*
+Averages.
+
+TODO : linear regression moving average,
+weighted moving average, moving median, ...
+https://en.wikipedia.org/wiki/Moving_average#Weighted_moving_average
+https://www.alglib.net/time-series/moving-average-filter.php
+https://www.incrediblecharts.com/indicators/linear_regression.php
+*/
+
+
+namespace fea {
+// Computes the cumulative average (rolling / running average).
+// Providing n makes the computation bounded, else it is unbounded (infinite).
+//
+// Nothing special here, your typical mean given N samples, but only stores 1
+// value.
+//
+// This can become quite imprecise, and it is recommended to use a different
+// moving average algorithm.
+template <class T>
+struct cumulative_average {
+	static constexpr bool is_int_v = std::is_integral_v<T>;
+	using mfloat_t = std::conditional_t<sizeof(T) == 8, double, float>;
+
+	cumulative_average(size_t n) noexcept
+			: _n(n) {
+	}
+	cumulative_average() noexcept = default;
+
+	T operator()(const T& in) {
+		// Unbounded.
+		if (_n == 0 || _size != _n) {
+			_last = (mfloat_t(in) + mfloat_t(_size) * _last)
+					/ mfloat_t(_size + 1);
+			++_size;
+			if constexpr (is_int_v) {
+				return T(std::round(_last));
+			} else {
+				return T(_last);
+			}
+		}
+
+		// Bounded.
+		_last -= _last / _n;
+		_last += mfloat_t(in) / _n;
+
+		if constexpr (is_int_v) {
+			return T(std::round(_last));
+		} else {
+			return T(_last);
+		}
+	}
+
+private:
+	size_t _n = 0;
+	size_t _size = 0;
+	mfloat_t _last = 0;
+};
+
+template <class T>
+using ca = cumulative_average<T>;
+
+
+// Computes the simple moving average of N samples.
+// Uncentered.
+template <class T, size_t N>
+struct simple_moving_average {
+	static constexpr bool is_int_v = std::is_integral_v<T>;
+	using mfloat_t = std::conditional_t<sizeof(T) == 8, double, float>;
+
+	simple_moving_average() noexcept = default;
+
+	T operator()(const T& in) {
+		if (_size != _circle_buf.size()) {
+			// cumulative
+			_circle_buf[_size++] = mfloat_t(in);
+			_last = fea::mean(_circle_buf.begin(), _circle_buf.begin() + _size);
+
+			if constexpr (is_int_v) {
+				return T(std::round(_last));
+			} else {
+				return T(_last);
+			}
+		}
+
+		// 1/k * (pn+1 - p1)
+		mfloat_t new_v = (mfloat_t(in) - _circle_buf[_playhead]) * _divider;
+		_last = _last + new_v;
+
+		_circle_buf[_playhead] = mfloat_t(in);
+		_playhead = _playhead + 1 == _circle_buf.size() ? 0 : _playhead + 1;
+
+		if constexpr (is_int_v) {
+			return T(std::round(_last));
+		} else {
+			return T(_last);
+		}
+	}
+
+private:
+	mfloat_t _divider = mfloat_t(1) / mfloat_t(N);
+	mfloat_t _last = mfloat_t(0);
+	size_t _playhead = 0;
+	size_t _size = 0;
+	std::array<mfloat_t, N> _circle_buf{};
+};
+
+template <class T, size_t N>
+using sma = simple_moving_average<T, N>;
+
+
+// Computes the exponential moving average.
+// Alpha defaults to 0.5 if it isn't provided.
+template <class T>
+struct exponential_moving_average {
+	static constexpr bool is_int_v = std::is_integral_v<T>;
+	using mfloat_t = std::conditional_t<sizeof(T) == 8, double, float>;
+
+	exponential_moving_average(mfloat_t alpha, T first_value) noexcept
+			: _alpha(alpha)
+			, _last(mfloat_t(first_value)) {
+		assert(_alpha > mfloat_t(0) && _alpha < mfloat_t(1));
+	}
+	exponential_moving_average(mfloat_t alpha) noexcept
+			: exponential_moving_average(alpha, mfloat_t(0)) {
+	}
+	exponential_moving_average() noexcept
+			: exponential_moving_average(mfloat_t(0.5), mfloat_t(0)) {
+	}
+
+	T operator()(const T& in) {
+		mfloat_t in_f = mfloat_t(in);
+		_last = (in_f * _alpha) + _last * _alpha_inv;
+		if constexpr (is_int_v) {
+			return T(std::round(_last));
+		} else {
+			return T(_last);
+		}
+	}
+
+private:
+	mfloat_t _alpha = 0.5;
+	mfloat_t _alpha_inv = mfloat_t(1) - _alpha;
+	mfloat_t _last = 0.0;
+};
+
+template <class T>
+using ema = exponential_moving_average<T>;
+
+
+} // namespace fea
---
+++ tests_cpp17/math/moving_average.cpp
@@ -0,0 +1,245 @@
+#include <cstdint>
+#include <fea/math/moving_average.hpp>
+#include <gtest/gtest.h>
+
+namespace {
+#define ERROR_MSG "average.cpp : Failed unit test."
+
+TEST(moving_average, ca_unbounded) {
+	static_assert(fea::ca<double>::is_int_v == false, ERROR_MSG);
+	static_assert(fea::ca<float>::is_int_v == false, ERROR_MSG);
+	static_assert(fea::ca<int64_t>::is_int_v == true, ERROR_MSG);
+	static_assert(fea::ca<int>::is_int_v == true, ERROR_MSG);
+
+	static_assert(std::is_same_v<typename fea::ca<double>::mfloat_t, double>,
+			ERROR_MSG);
+	static_assert(std::is_same_v<typename fea::ca<float>::mfloat_t, float>,
+			ERROR_MSG);
+	static_assert(std::is_same_v<typename fea::ca<int64_t>::mfloat_t, double>,
+			ERROR_MSG);
+	static_assert(
+			std::is_same_v<typename fea::ca<int>::mfloat_t, float>, ERROR_MSG);
+
+	// unbounded
+	{
+		fea::cumulative_average<double> ca;
+		EXPECT_EQ(ca(0.5), 0.5);
+		EXPECT_EQ(ca(0.5), 0.5);
+		EXPECT_EQ(ca(0.5), 0.5);
+		EXPECT_EQ(ca(0.5), 0.5);
+		EXPECT_EQ(ca(0.5), 0.5);
+		EXPECT_EQ(ca(0.5), 0.5);
+		EXPECT_EQ(ca(0.5), 0.5);
+		EXPECT_EQ(ca(0.5), 0.5);
+		EXPECT_EQ(ca(0.5), 0.5);
+		EXPECT_EQ(ca(0.5), 0.5);
+		EXPECT_EQ(ca(0.5), 0.5);
+		EXPECT_EQ(ca(0.5), 0.5);
+		EXPECT_EQ(ca(0.5), 0.5);
+		EXPECT_EQ(ca(0.5), 0.5);
+		EXPECT_EQ(ca(0.5), 0.5);
+		EXPECT_EQ(ca(0.5), 0.5);
+	}
+
+	{
+		fea::ca<double> ca;
+		EXPECT_EQ(ca(1.0), 1.0);
+		EXPECT_EQ(ca(0.5), 0.75);
+		EXPECT_EQ(ca(0.5), 1.0 / 1.5);
+		EXPECT_EQ(ca(0.5), 0.625);
+		EXPECT_EQ(ca(0.5), 0.6);
+		ca(0.5);
+		ca(0.5);
+		EXPECT_EQ(ca(0.5), 0.5625);
+	}
+
+	{
+		fea::ca<int> ca;
+		EXPECT_EQ(ca(1000), 1000);
+		EXPECT_EQ(ca(500), 750);
+		EXPECT_EQ(ca(500), 667);
+		EXPECT_EQ(ca(500), 625);
+		EXPECT_EQ(ca(500), 600);
+		EXPECT_EQ(ca(500), 583);
+	}
+
+	// bounded
+	{
+		fea::cumulative_average<double> ca(2);
+		EXPECT_EQ(ca(0.5), 0.5);
+		EXPECT_EQ(ca(0.5), 0.5);
+		EXPECT_EQ(ca(0.5), 0.5);
+		EXPECT_EQ(ca(0.5), 0.5);
+	}
+
+	{
+		fea::ca<double> ca(2);
+		EXPECT_EQ(ca(1.0), 1.0);
+		EXPECT_EQ(ca(0.5), 0.75);
+		EXPECT_EQ(ca(0.5), 0.625);
+		EXPECT_EQ(ca(0.5), 0.5625);
+		EXPECT_EQ(ca(0.5), 0.53125);
+		EXPECT_EQ(ca(0.5), 0.515625);
+	}
+
+	{
+		fea::ca<double> ca(5);
+		EXPECT_EQ(ca(1.0), 1.0);
+		EXPECT_EQ(ca(0.5), 0.75);
+		EXPECT_EQ(ca(0.5), 1.0 / 1.5);
+		EXPECT_EQ(ca(0.5), 0.625);
+		EXPECT_EQ(ca(0.5), 0.6);
+		EXPECT_EQ(ca(0.5), 0.58);
+	}
+
+	{
+		fea::ca<int> ca(2);
+		EXPECT_EQ(ca(1000), 1000);
+		EXPECT_EQ(ca(500), 750);
+		EXPECT_EQ(ca(500), 625);
+		EXPECT_EQ(ca(500), 563);
+		EXPECT_EQ(ca(500), 531);
+		EXPECT_EQ(ca(500), 516);
+	}
+
+	{
+		fea::ca<int> ca(5);
+		EXPECT_EQ(ca(1000), 1000);
+		EXPECT_EQ(ca(500), 750);
+		EXPECT_EQ(ca(500), 667);
+		EXPECT_EQ(ca(500), 625);
+		EXPECT_EQ(ca(500), 600);
+		EXPECT_EQ(ca(500), 580);
+	}
+}
+
+
+TEST(moving_average, sma) {
+	static_assert(fea::sma<double, 10>::is_int_v == false, ERROR_MSG);
+	static_assert(fea::sma<float, 10>::is_int_v == false, ERROR_MSG);
+	static_assert(fea::sma<int64_t, 10>::is_int_v == true, ERROR_MSG);
+	static_assert(fea::sma<int, 10>::is_int_v == true, ERROR_MSG);
+
+	static_assert(
+			std::is_same_v<typename fea::sma<double, 10>::mfloat_t, double>,
+			ERROR_MSG);
+	static_assert(std::is_same_v<typename fea::sma<float, 10>::mfloat_t, float>,
+			ERROR_MSG);
+	static_assert(
+			std::is_same_v<typename fea::sma<int64_t, 10>::mfloat_t, double>,
+			ERROR_MSG);
+	static_assert(std::is_same_v<typename fea::sma<int, 10>::mfloat_t, float>,
+			ERROR_MSG);
+
+	{
+		fea::simple_moving_average<double, 2> sma;
+		EXPECT_EQ(sma(0.5), 0.5);
+		EXPECT_EQ(sma(0.5), 0.5);
+		EXPECT_EQ(sma(0.5), 0.5);
+		EXPECT_EQ(sma(0.5), 0.5);
+	}
+
+	{
+		fea::simple_moving_average<double, 2> sma;
+		EXPECT_EQ(sma(1.0), 1.0);
+		EXPECT_EQ(sma(0.5), 0.75);
+		EXPECT_EQ(sma(0.5), 0.5);
+		EXPECT_EQ(sma(0.5), 0.5);
+		EXPECT_EQ(sma(0.5), 0.5);
+		EXPECT_EQ(sma(0.5), 0.5);
+		EXPECT_EQ(sma(1.0), 0.75);
+		EXPECT_EQ(sma(1.0), 1.0);
+		EXPECT_EQ(sma(0.5), 0.75);
+		EXPECT_EQ(sma(0.5), 0.5);
+		EXPECT_EQ(sma(0.5), 0.5);
+	}
+
+	{
+		fea::simple_moving_average<double, 5> sma;
+		EXPECT_EQ(sma(1.0), 1.0);
+		EXPECT_EQ(sma(0.5), 0.75);
+		EXPECT_EQ(sma(0.5), 1.0 / 1.5);
+		EXPECT_EQ(sma(0.5), 0.625);
+		EXPECT_EQ(sma(0.5), 0.6);
+		EXPECT_EQ(sma(0.5), 0.5);
+	}
+
+	{
+		fea::sma<int, 2> sma;
+		EXPECT_EQ(sma(1000), 1000);
+		EXPECT_EQ(sma(500), 750);
+		EXPECT_EQ(sma(500), 500);
+		EXPECT_EQ(sma(500), 500);
+		EXPECT_EQ(sma(500), 500);
+		EXPECT_EQ(sma(500), 500);
+		EXPECT_EQ(sma(500), 500);
+		EXPECT_EQ(sma(1000), 750);
+		EXPECT_EQ(sma(1000), 1000);
+		EXPECT_EQ(sma(500), 750);
+		EXPECT_EQ(sma(500), 500);
+		EXPECT_EQ(sma(500), 500);
+		EXPECT_EQ(sma(500), 500);
+		EXPECT_EQ(sma(500), 500);
+	}
+
+	{
+		fea::sma<int, 5> sma;
+		EXPECT_EQ(sma(1000), 1000);
+		EXPECT_EQ(sma(500), 750);
+		EXPECT_EQ(sma(500), 667);
+		EXPECT_EQ(sma(500), 625);
+		EXPECT_EQ(sma(500), 600);
+		EXPECT_EQ(sma(500), 500);
+	}
+}
+
+TEST(moving_average, ema) {
+	static_assert(fea::ema<double>::is_int_v == false, ERROR_MSG);
+	static_assert(fea::ema<float>::is_int_v == false, ERROR_MSG);
+	static_assert(fea::ema<int64_t>::is_int_v == true, ERROR_MSG);
+	static_assert(fea::ema<int>::is_int_v == true, ERROR_MSG);
+
+	static_assert(std::is_same_v<typename fea::ema<double>::mfloat_t, double>,
+			ERROR_MSG);
+	static_assert(std::is_same_v<typename fea::ema<float>::mfloat_t, float>,
+			ERROR_MSG);
+	static_assert(std::is_same_v<typename fea::ema<int64_t>::mfloat_t, double>,
+			ERROR_MSG);
+	static_assert(
+			std::is_same_v<typename fea::ema<int>::mfloat_t, float>, ERROR_MSG);
+
+	{
+		// Alpha 0.5, init avg 0.0.
+		fea::exponential_moving_average<double> ema;
+		EXPECT_EQ(ema(0.5), 0.25);
+		EXPECT_EQ(ema(0.5), 0.375);
+		EXPECT_EQ(ema(0.5), 0.4375);
+		EXPECT_EQ(ema(0.5), 0.46875);
+		EXPECT_EQ(ema(0.5), 0.484375);
+		EXPECT_EQ(ema(0.5), 0.4921875);
+	}
+
+	{
+		fea::ema<double> ema{ 0.5, 1.0 };
+		EXPECT_EQ(ema(1.0), 1.0);
+		EXPECT_EQ(ema(1.0), 1.0);
+		EXPECT_EQ(ema(0.5), 0.75);
+		EXPECT_EQ(ema(0.5), 0.625);
+		EXPECT_EQ(ema(0.5), 0.5625);
+		EXPECT_EQ(ema(0.5), 0.53125);
+		EXPECT_EQ(ema(0.5), 0.515625);
+		EXPECT_EQ(ema(0.5), 0.5078125);
+	}
+
+	{
+		fea::ema<int> ema{ 0.5, 1000 };
+		EXPECT_EQ(ema(1000), 1000);
+		EXPECT_EQ(ema(500), 750);
+		EXPECT_EQ(ema(500), 625);
+		EXPECT_EQ(ema(500), 563);
+		EXPECT_EQ(ema(500), 531);
+		EXPECT_EQ(ema(500), 516);
+		EXPECT_EQ(ema(500), 508);
+	}
+}
+} // namespace
