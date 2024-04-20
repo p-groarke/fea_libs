fe798830e7af580557b54342fcfe2e01a2569aa4
Philippe Groarke <hello@philippegroarke.com>
2024-04-20 14:13:24 -0400

    ci

--- tests_cpp14/enum/utility.cpp
+++ tests_cpp14/enum/utility.cpp
@@ -4,7 +4,7 @@
 #include <gtest/gtest.h>
 
 namespace {
-#define TEST_FAIL_MSG "utility.cpp : Unit test failed."
+#define FAILMSG "utility.cpp : Unit test failed."
 
 enum class e1 : uint8_t {
 	bla,
@@ -23,13 +23,13 @@ TEST(enum_utility, basics) {
 	using namespace fea::literals;
 
 	using ret1_t = decltype(fea::to_underlying(e1{}));
-	static_assert(std::is_same<ret1_t, uint8_t>::value, TEST_FAIL_MSG);
+	static_assert(std::is_same<ret1_t, uint8_t>::value, FAILMSG);
 
 	using ret2_t = decltype(fea::to_underlying(e2{}));
-	static_assert(std::is_same<ret2_t, uint16_t>::value, TEST_FAIL_MSG);
+	static_assert(std::is_same<ret2_t, uint16_t>::value, FAILMSG);
 
 	using ret3_t = decltype(fea::to_underlying(e3{}));
-	static_assert(std::is_same<ret3_t, int>::value, TEST_FAIL_MSG);
+	static_assert(std::is_same<ret3_t, int>::value, FAILMSG);
 
 	EXPECT_EQ(fea::to_underlying(e1::bla), 0_u8);
 	EXPECT_EQ(fea::to_underlying(e1::blee), 1_u8);
--- tests_cpp14/performance/constants.cpp
+++ tests_cpp14/performance/constants.cpp
@@ -4,28 +4,25 @@
 
 #if FEA_WITH_TBB
 namespace {
-#define TEST_FAIL_MSG "constants.cpp : Unit test failed."
+#define FAILMSG "constants.cpp : Unit test failed."
 
 TEST(constants, basics) {
-	static_assert(
-			fea::default_grainsize_small<true>::value == 1u, TEST_FAIL_MSG);
-	static_assert(fea::default_grainsize_small_v<true> == 1u, TEST_FAIL_MSG);
+	static_assert(fea::default_grainsize_small<true>::value == 1u, FAILMSG);
+	static_assert(fea::default_grainsize_small_v<true> == 1u, FAILMSG);
 
-	static_assert(
-			fea::default_grainsize_medium<true>::value == 100u, TEST_FAIL_MSG);
-	static_assert(fea::default_grainsize_medium_v<true> == 100u, TEST_FAIL_MSG);
+	static_assert(fea::default_grainsize_medium<true>::value == 100u, FAILMSG);
+	static_assert(fea::default_grainsize_medium_v<true> == 100u, FAILMSG);
 
-	static_assert(
-			fea::default_grainsize_large<true>::value == 1000u, TEST_FAIL_MSG);
-	static_assert(fea::default_grainsize_large_v<true> == 1000u, TEST_FAIL_MSG);
+	static_assert(fea::default_grainsize_large<true>::value == 1000u, FAILMSG);
+	static_assert(fea::default_grainsize_large_v<true> == 1000u, FAILMSG);
 
 	using default_partitioner_t = typename fea::default_partitioner<true>::type;
 	static_assert(
 			std::is_same<default_partitioner_t, tbb::static_partitioner>::value,
-			TEST_FAIL_MSG);
+			FAILMSG);
 	static_assert(std::is_same<fea::default_partitioner_t<true>,
 						  tbb::static_partitioner>::value,
-			TEST_FAIL_MSG);
+			FAILMSG);
 }
 } // namespace
 #endif
