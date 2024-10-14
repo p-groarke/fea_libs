0303a7f93fafd3d791f1cd6039b3288b61500778
Philippe Groarke <hello@philippegroarke.com>
2024-10-14 14:09:14 -0400

    refactoring

--- tests_cpp17/functional/func_ptr.cpp
+++ tests_cpp17/functional/func_ptr.cpp
@@ -45,6 +45,35 @@ TEST(func_ptr, c_basics) {
 	}
 
 	{
+		fea::func_ptr<int()> f([]() { return 101; });
+		EXPECT_TRUE(f);
+		EXPECT_EQ(f(), 101);
+		EXPECT_EQ(f.to_function()(), 101);
+	}
+
+	{
+		constexpr fea::func_ptr<int()> f{ []() -> int { return 101; } };
+		EXPECT_TRUE(f);
+		EXPECT_EQ(f(), 101);
+		EXPECT_EQ(f.to_function()(), 101);
+#if FEA_CPP20
+		static_assert(f() == 101, FAIL_MSG);
+#endif
+	}
+
+	{
+		constexpr fea::func_ptr<int(int, int)> f{ [](int r, int) {
+			return r;
+		} };
+		EXPECT_TRUE(f);
+		EXPECT_EQ(f(42, 101), 42);
+		EXPECT_EQ(f.to_function()(42, 101), 42);
+#if FEA_CPP20
+		static_assert(f(42, 101) == 42, FAIL_MSG);
+#endif
+	}
+
+	{
 		// These should compile.
 		constexpr fea::func_ptr<int(int)> f1 = &cfunc;
 		constexpr fea::func_ptr<int(const int&)> f2 = &cfunc;
@@ -175,4 +204,84 @@ TEST(func_ptr, member_basics) {
 	}
 }
 
+// FROM fea::function_cl
+// TODO : Make these work
+
+// enum class ret {
+//	mem_func,
+//	const_mem_func,
+//	static_mem_func,
+//	free_func1,
+//	free_func2,
+//	lambda1,
+//	lambda2,
+//	lambda3,
+//};
+//
+// struct test_obj {
+//	ret func() {
+//		return ret::mem_func;
+//	}
+//	ret func_const() const {
+//		return ret::const_mem_func;
+//	}
+//
+//	static ret static_func() {
+//		return ret::static_mem_func;
+//	}
+//};
+//
+// ret free_func(test_obj*) {
+//	return ret::free_func1;
+//}
+//
+// ret free_func2() {
+//	return ret::free_func2;
+//}
+//
+// TEST(function_cl, basics) {
+//	{
+//		test_obj o;
+//		fea::func_ptr<ret(test_obj*)> f;
+//		EXPECT_FALSE(f);
+//
+//		f = &test_obj::func;
+//		EXPECT_TRUE(f);
+//		EXPECT_EQ(f(&o), ret::mem_func);
+//
+//		f = &test_obj::func_const;
+//		EXPECT_EQ(f(&o), ret::const_mem_func);
+//
+//		f = &free_func;
+//		EXPECT_EQ(f(&o), ret::free_func1);
+//
+//		f = [](test_obj*) -> ret { return ret::lambda1; };
+//		EXPECT_EQ(f(&o), ret::lambda1);
+//
+//		auto l = [](test_obj*) -> ret { return ret::lambda2; };
+//		f = l;
+//		EXPECT_EQ(f(&o), ret::lambda2);
+//
+//		f = {};
+//		EXPECT_FALSE(f);
+//	}
+//
+//	{
+//		fea::func_ptr<ret()> f;
+//		EXPECT_FALSE(f);
+//
+//		f = &test_obj::static_func;
+//		EXPECT_TRUE(f);
+//		EXPECT_EQ(f(), ret::static_mem_func);
+//
+//		f = &free_func2;
+//		EXPECT_EQ(f(), ret::free_func2);
+//
+//		f = []() { return ret::lambda3; };
+//		EXPECT_EQ(f(), ret::lambda3);
+//
+//		f = {};
+//		EXPECT_FALSE(f);
+//	}
+//}
 } // namespace
